bd915590a1c996206e0f86109996ec12ac5a13c9
MattSegal <mattdsegal@gmail.com>
2016-05-17 19:38:40 +1000

    breakout from webwalrus

--- __init__.py
+++ __init__.py
@@ -1,9 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Flask, Blueprint, render_template
 
-visual_algo_app = Blueprint('visual_algo_app', __name__,
+visual_algo = Blueprint('visual_algo', __name__,
 			template_folder='templates', 
-			static_folder='static', static_url_path = '/visual_algo_app')
+			static_folder='static', static_url_path = '/visual_algo/static')
 
-@visual_algo_app.route('/sort/')
-def  sort():
-    return render_template('index.html')
+@visual_algo.route('/sort/')
+def visual_sort():
+    return render_template('visual_sort.html')
+
+if __name__ == "__main__":
+    app = Flask(__name__)
+    app.register_blueprint(visual_algo)
+    app.run(host= '0.0.0.0',debug=True)
