2fdb54b9f46acb143d8a8e7db336736bb0554ded
MattSegal <mattdsegal@gmail.com>
2016-11-19 17:01:33 +1100

    Add gulpfile Make compatible with fabric deployment tool.

--- __init__.py
+++ __init__.py
@@ -1,14 +1,10 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, render_template
 
-visual_algo = Blueprint('visual_algo', __name__,
-			template_folder='templates', 
-			static_folder='static', static_url_path = '/visual_algo/static')
+app = Flask(__name__)
 
-@visual_algo.route('/sort/')
+@app.route('/sort/')
 def visual_sort():
     return render_template('visual_sort.html')
 
 if __name__ == "__main__":
-    app = Flask(__name__)
-    app.register_blueprint(visual_algo)
-    app.run(host= '0.0.0.0',debug=True)
+    app.run(host='0.0.0.0',debug=True)
